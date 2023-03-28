d6d7226e6ba4e14f0f578b154a95f6e38d1637e4
ingaSiu <ingasiud@gmail.com>
2023-03-28 19:42:01 +0300

    added required imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import './index.css';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Toaster } from 'react-hot-toast';
+import { UserProvider } from './context/UserContext';
+
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <UserProvider>
+          <App />
+        </UserProvider>
+
+        <Toaster position="bottom-left" />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
-)
+);
+
