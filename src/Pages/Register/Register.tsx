1618a5b6c017330c5ef020fdfef21cd73b2784ec
ingaSiu <ingasiud@gmail.com>
2023-03-28 19:47:52 +0300

    Register page

--- src/Pages/Register/Register.tsx
+++ src/Pages/Register/Register.tsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+
+import { HOME_PATH } from '../../routes/const';
+import RegisterForm from './RegisterForm';
+import { UserContext } from '../../context/UserContext';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
-  return <div>Register</div>;
+  const { isLoggedIn } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      navigate(HOME_PATH);
+    }
+  }, [isLoggedIn]);
+  return (
+    <Wrapper>
+      <Title>Register here! </Title>
+      <RegisterForm />
+    </Wrapper>
+  );
 };
 
 export default Register;
+
+const Title = styled.h1`
+  font-size: 40px;
+  padding: 20px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
