6fb385f1dcef9e5694490b45a8976fc4617e1b84
ingaSiu <ingasiud@gmail.com>
2023-03-28 19:49:23 +0300

    menu with two different navbar containers

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,8 +1,22 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
+import Button from './Button';
+import { LOGIN_PATH } from '../routes/const';
+import { LightGrey } from '../const/styles';
+import { UserContext } from '../context/UserContext';
 import logo from '../assets/images/logo.png';
 import styled from 'styled-components';
+import { toast } from 'react-hot-toast';
+import { useContext } from 'react';
 
 const Menu = () => {
+  const { isLoggedIn, setUser } = useContext(UserContext);
+  const navigate = useNavigate();
+  const logOut = () => {
+    setUser(null);
+    navigate(LOGIN_PATH);
+    toast.success('Logged out succesfully');
+  };
   return (
     <>
       <Wrapper>
@@ -13,22 +27,22 @@ const Menu = () => {
           <IconText>Super Exam</IconText>
         </NavbarLinkContainer>
 
-        <NavbarLinkContainer>
-          <NavbarLink to={'/'}>HOME</NavbarLink>
-          <NavbarLink to={'/add'}>ADD</NavbarLink>
-        </NavbarLinkContainer>
-
-        <NavbarLinkContainer>
-          <NavbarLink to={'/register'}>REGISTER</NavbarLink>
-          <NavbarLink to={'/login'}>LOGIN</NavbarLink>
-        </NavbarLinkContainer>
-
-        {/* {!isLoggenIn && <TestComponent />}
         {isLoggedIn && (
-          <ButtonLink to={'/'} onClick={onClick}>
-            <ButtonStyle>Logout</ButtonStyle>
-          </ButtonLink>
-        )} */}
+          <NavbarLinkContainer>
+            <NavbarLink to={'/'}>HOME</NavbarLink>
+            <NavbarLink to={'/add'}>ADD</NavbarLink>
+            <BtnWrapper>
+              <Button onClick={logOut} title="Logout" />
+            </BtnWrapper>
+          </NavbarLinkContainer>
+        )}
+
+        {!isLoggedIn && (
+          <NavbarLinkContainer>
+            <NavbarLink to={'/register'}>REGISTER</NavbarLink>
+            <NavbarLink to={'/login'}>LOGIN</NavbarLink>
+          </NavbarLinkContainer>
+        )}
       </Wrapper>
     </>
   );
@@ -41,8 +55,8 @@ const Wrapper = styled.div`
   background-color: #fff;
   display: flex;
   align-items: center;
-  border-top: 1px solid #f4f4f4;
-  border-bottom: 1px solid #f4f4f4;
+  border-top: 1px solid ${LightGrey};
+  border-bottom: 1px solid ${LightGrey};
   justify-content: space-around;
   margin-top: 10px;
 `;
@@ -51,6 +65,7 @@ const NavbarLinkContainer = styled.div`
   display: flex;
   text-decoration: none;
   color: black;
+  align-items: center;
 `;
 
 const NavbarLink = styled(Link)`
@@ -72,4 +87,9 @@ const IconContainer = styled.div`
 
 const IconText = styled.p`
   font-size: 20px;
+`;
+
+const BtnWrapper = styled.div`
+  margin-bottom: 15px;
+  margin-left: 25px;
 `;
